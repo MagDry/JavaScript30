dcf2bf257a7b8a16c4aa5a5b9c181fc131825bd9
MagDry <magdalena.drymajlo@gmail.com>
2018-11-09 03:35:02 +0100

    3done

--- 00 - COMPLETE/03 - CSS Variables/appMyWork.js
+++ 00 - COMPLETE/03 - CSS Variables/appMyWork.js
@@ -5,8 +5,11 @@ document.addEventListener('DOMContentLoaded', function(){
 
     function handleUpdate() {
         // console.log(this.value);
-        const suffix = this.dataset;
-        console.log(this.dataset);
+        const suffix = this.dataset.sizing || '';
+        // console.log(this.dataset);
+        // console.log(suffix);
+        // console.log(this.name);
+        document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
     }
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
