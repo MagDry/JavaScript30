ca100fcd7d0fbc1eff71820c4387edcac6acc52a
MagDry <magdalena.drymajlo@gmail.com>
2018-12-10 02:07:27 +0100

    ranges

--- 11 - Custom Video Player/scriptsMyWork.js
+++ 11 - Custom Video Player/scriptsMyWork.js
@@ -27,5 +27,31 @@ function togglePlay() {
     // video[method]();
 };
 
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    // console.log(icon);
+    toggle.textContent = icon;
+    // console.log('Update the butto');
+};
+
+function skip() {
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+};
+
+function handleRangeUpdate() {
+    console.log(this.value);
+};
+
 
 //Hook up the event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
